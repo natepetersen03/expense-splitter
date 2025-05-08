622a7a1531ed603b11546641df5deef8bc3ef116
Petersen, Nathaniel Loyd Atella <npetersen3@gatech.edu>
2025-05-08 11:03:20 -0600

    Designed data model

---
+++ Expense Splitter/Expense Splitter/Expense_SplitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Expense_SplitterApp.swift
+//  Expense Splitter
+//
+//  Created by Nate Petersen on 5/8/25.
+//
+
+import SwiftUI
+
+@main
+struct Expense_SplitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
